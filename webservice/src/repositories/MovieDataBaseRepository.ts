45ec6a3110e0b947b8bc658d6fe6c3aa57e9e72e
Lyman McBride <lyman.mcbride@jobnimbus.com>
2022-05-20 08:43:44 -0600

    add params as object in repo

--- webservice/src/repositories/MovieDataBaseRepository.ts
+++ webservice/src/repositories/MovieDataBaseRepository.ts
@@ -1,16 +1,25 @@
 import axios, {AxiosResponse} from "axios";
 
-class MovieDataBaseRepository{
+class MovieDataBaseRepository {
     movieDbBaseUrl = 'https://api.themoviedb.org/3';
     apiKey = '2b22e9de3ed842ea6bb39ee46e590a98';
+    language = 'en-US';
 
     public searchMovies = async (searchParameter: string) => {
-        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/search/movie?api_key=${this.apiKey}&language=en-US&query=${searchParameter}&page=1&include_adult=false`);
+        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/search/movie`, {
+            params: {
+                api_key: this.apiKey,
+                language: this.language,
+                query: searchParameter,
+                page: 1,
+                include_adult: false
+            }
+        });
         return result.data;
     };
 
     public getConfiguration = async () => {
-        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/configuration?api_key=${this.apiKey}`);
+        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/configuration`, {params: {api_key: this.apiKey}});
         return result.data;
     };
 }
