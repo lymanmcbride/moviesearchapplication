666725f8815846a07e0c284f26556008301812d4
Lyman McBride <lyman.mcbride@jobnimbus.com>
2022-05-20 08:16:44 -0600

    add logic for retriving base image url

--- webservice/src/repositories/MovieDataBaseRepository.ts
+++ webservice/src/repositories/MovieDataBaseRepository.ts
@@ -1,10 +1,16 @@
 import axios, {AxiosResponse} from "axios";
 
 class MovieDataBaseRepository{
-    movieDbBaseUrl = 'https://api.themoviedb.org';
+    movieDbBaseUrl = 'https://api.themoviedb.org/3';
+    apiKey = '2b22e9de3ed842ea6bb39ee46e590a98';
 
     public searchMovies = async (searchParameter: string) => {
-        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/3/search/movie?api_key=2b22e9de3ed842ea6bb39ee46e590a98&language=en-US&query=${searchParameter}&page=1&include_adult=false`);
+        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/search/movie?api_key=${this.apiKey}&language=en-US&query=${searchParameter}&page=1&include_adult=false`);
+        return result.data;
+    };
+
+    public getConfiguration = async () => {
+        let result: AxiosResponse = await axios.get(`${this.movieDbBaseUrl}/configuration?api_key=${this.apiKey}`);
         return result.data;
     };
 }
--- webservice/src/services/movies.service.ts
+++ webservice/src/services/movies.service.ts
@@ -1,21 +1,30 @@
 import MovieDataBaseRepository from "../repositories/MovieDataBaseRepository";
 import {MovieDataBaseSearchResult} from "../models/ExternalDtos/MovieDataBaseSearchResult";
 import {MovieSearchOutputDto} from "../models/InternalDtos/MovieSearchOutputDto";
+import {ConfigurationDto} from "../models/ExternalDtos/ConfigurationDto";
 
 class MoviesService {
     public getMoviesInfo = (searchParameter: string) => {
+        let baseImageUrl: string;
+        this.getBaseImageUrl().then((url) => baseImageUrl = url);
         return this.getMoviesData(searchParameter)
-            .then((movieDataBaseSearchResult: MovieDataBaseSearchResult) => this.mapToSingleMovieObject(movieDataBaseSearchResult));
+            .then((movieDataBaseSearchResult: MovieDataBaseSearchResult) => this.mapToMovieSearchOutputArray(movieDataBaseSearchResult, baseImageUrl));
     };
 
-    private mapToSingleMovieObject(movieDataBaseSearchResult: MovieDataBaseSearchResult): MovieSearchOutputDto[] {
+    private getBaseImageUrl() {
+        return MovieDataBaseRepository
+            .getConfiguration()
+            .then((configurationDto: ConfigurationDto) => Promise.resolve(`${configurationDto.images.secure_base_url}${configurationDto.images.logo_sizes[1]}`))
+    }
+
+    private static mapToMovieSearchOutputArray(movieDataBaseSearchResult: MovieDataBaseSearchResult, baseImageUrl: string): MovieSearchOutputDto[] {
         let movieSearchOutput: MovieSearchOutputDto[] = [];
         for (let i = 0; i < 10; i++){
             let movie = movieDataBaseSearchResult.results[i];
             movieSearchOutput.push({
                 movie_id: movie.id,
                 title: movie.title,
-                poster_image: movie.poster_path,
+                poster_image: `${baseImageUrl}${movie.poster_path}`,
                 popularity_summary: `Popularity: ${movie.popularity}. Number of Votes: ${movie.vote_count}`
             })
         }
